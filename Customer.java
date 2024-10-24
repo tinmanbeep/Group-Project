5ee6fed6bdce0a8d39cc2fba5a177189a1a8a99d
tinmanbeep <elijahslatson@gmail.com>
2024-10-24 16:35:16 -0400

    Update Customer.java
    Added getters, setters, constructors

--- Customer.java
+++ Customer.java
@@ -1,4 +1,4 @@
-package csc222Quiz_m5StartFiles;
+package timesharerentals;
 
 public class Customer {
 	
@@ -12,7 +12,14 @@ public class Customer {
 	private static int nextNum = 100;
 	
 	
-	// create your constructors
+	public Customer() {
+		
+	}
+	
+	public Customer(String name, boolean premier) {
+		this.name = name;
+		this.premier = premier;
+	}
 	
 	// toString method
 	public String toString()
@@ -22,8 +29,48 @@ public class Customer {
 			ans = " is not a";
 		return name + " (custID: " + custId + ") " + ans + " premier member of the club";
 	}
+
+
+	public int getCustId() {
+		return custId;
+	}
+
+
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public boolean isPremier() {
+		return premier;
+	}
+
+
+	public void setPremier(boolean premier) {
+		this.premier = premier;
+	}
+
+
+	public static int getNextNum() {
+		return nextNum;
+	}
+
+
+	public static void setNextNum(int nextNum) {
+		Customer.nextNum = nextNum;
+	}
 	
-	//add getters and setters
+	
 
 
 }
