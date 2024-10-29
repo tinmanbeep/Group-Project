c55f0428033f4e23e77cdc91b4e031077fd097ab
infrin <40410267+infrin@users.noreply.github.com>
2024-10-29 02:53:35 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,21 +2,26 @@ package timesharerentals;
 
 public class Customer {
 	
-	
+	// the Customer class has three fields
 	private int custId;
 	private String name;
-   	private boolean premier;
+    	private boolean premier;
 	
-	
+	// the following will be used to automatically generate customer ids
+	//so they will be unique
+	private static int nextNum = 100;
 	
 	public Customer() {
 		
 	}
 	
-	public Customer(String name, boolean premier,int custId) {
+	public Customer(String name) {
+		this.name = name;
+	}
+	
+	public Customer(String name, boolean premier) {
 		this.name = name;
 		this.premier = premier;
-		this.custId = custId;
 	}
 	
 	// toString method
@@ -24,46 +29,39 @@ public class Customer {
 	{
 		String ans = "is a";
 		if(!premier)
-			ans = " is not a";
+			ans = "is not a";
 		return name + " (custID: " + custId + ") " + ans + " premier member of the club";
 	}
 
-
 	public int getCustId() {
-		return custId;
+		return this.custId;
 	}
-
 
 	public void setCustId(int custId) {
 		this.custId = custId;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public boolean isPremier() {
 		return premier;
 	}
 
-
 	public void setPremier(boolean premier) {
 		this.premier = premier;
 	}
-	
-	public static int getCustId() {
-		return custId;
-	}
-	
-	public static void setCustId(int custId) {
-		this.custId = custId;
+
+	public static int getNextNum() {
+		return nextNum;
 	}
 
+	public static void setNextNum(int nextNum) {
+		Customer.nextNum = nextNum;
+	}
 }
